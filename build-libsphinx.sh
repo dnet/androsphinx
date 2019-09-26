e206527b1f9d52b2752444989741177d85c2bbc4
András Veres-Szentkirályi <vsza@vsza.hu>
2019-09-26 21:45:35 +0200

    build-libsphinx.sh: extracted function compile_libsphinx_arch

--- build-libsphinx.sh
+++ build-libsphinx.sh
@@ -6,58 +6,32 @@ SPHINX=libsphinx
 SODIUM=libsodium_headers
 OUTDIR=app/src/main/jniLibs
 
+compile_libsphinx_arch() {
+	ARCH=$1
+	TARGET=../../$OUTDIR/$ARCH
+	cd goldilocks
+	make clean
+	make $4 android_$2
+	cd ..
+	cp ../../app/build/intermediates/merged_native_libs/debug/out/lib/$ARCH/libsodiumjni.so .
+	make clean
+	make CC=$3 SODIUM=../../$SODIUM android
+	mkdir -p $TARGET
+	cp libsphinx.so $TARGET
+}
+
 git submodule update --init --recursive --remote
 
 rm -rf $SODIUM
 mkdir $SODIUM
 ln -s /usr/include/sodium* $SODIUM
 
-ARCH=x86_64
-TARGET=../../$OUTDIR/$ARCH
-cd $SPHINX/src/goldilocks
-git submodule update --init --recursive --remote
-make android_x86_64
-cd ..
-cp ../../app/build/intermediates/merged_native_libs/debug/out/lib/$ARCH/libsodiumjni.so .
-make CC=x86_64-linux-android21-clang SODIUM=../../$SODIUM android
-mkdir -p $TARGET
-cp libsphinx.so $TARGET
+cd $SPHINX/src
 
-ARCH=x86
-TARGET=../../$OUTDIR/$ARCH
-cd goldilocks
-make clean
-make FIELD_ARCH=arch_32 android_i686
-cd ..
-make clean
-cp ../../app/build/intermediates/merged_native_libs/debug/out/lib/$ARCH/libsodiumjni.so .
-make CC=i686-linux-android21-clang SODIUM=../../$SODIUM android
-mkdir -p $TARGET
-cp libsphinx.so $TARGET
-
-ARCH=arm64-v8a
-TARGET=../../$OUTDIR/$ARCH
-cd goldilocks
-make clean
-make FIELD_ARCH=arch_ref64 android_aarch64
-cd ..
-make clean
-cp ../../app/build/intermediates/merged_native_libs/debug/out/lib/$ARCH/libsodiumjni.so .
-make CC=aarch64-linux-android21-clang SODIUM=../../$SODIUM android
-mkdir -p $TARGET
-cp libsphinx.so $TARGET
-
-ARCH=armeabi-v7a
-TARGET=../../$OUTDIR/$ARCH
-cd goldilocks
-make clean
-make FIELD_ARCH=arch_32 android_armv7a
-cd ..
-make clean
-cp ../../app/build/intermediates/merged_native_libs/debug/out/lib/$ARCH/libsodiumjni.so .
-make CC=armv7a-linux-androideabi21-clang SODIUM=../../$SODIUM android
-mkdir -p $TARGET
-cp libsphinx.so $TARGET
+compile_libsphinx_arch "x86_64"      "x86_64"      "x86_64-linux-android21-clang"
+compile_libsphinx_arch "x86"         "i686"          "i686-linux-android21-clang" "FIELD_ARCH=arch_32"
+compile_libsphinx_arch "arm64-v8a"   "aarch64"    "aarch64-linux-android21-clang" "FIELD_ARCH=arch_ref64"
+compile_libsphinx_arch "armeabi-v7a" "armv7a"  "armv7a-linux-androideabi21-clang" "FIELD_ARCH=arch_32"
 
 cd ../..
 rm -rf $SODIUM
