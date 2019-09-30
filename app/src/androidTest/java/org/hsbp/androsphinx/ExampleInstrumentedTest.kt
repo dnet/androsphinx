f7c22f053a1d4a3286ae7fb346f30c97dffbcb35
András Veres-Szentkirályi <vsza@vsza.hu>
2019-09-30 11:02:11 +0200

    added tests for Android local storage

--- app/src/androidTest/java/org/hsbp/androsphinx/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/hsbp/androsphinx/ExampleInstrumentedTest.kt
@@ -2,11 +2,14 @@ package org.hsbp.androsphinx
 
 import android.util.Base64
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.libsodium.jni.NaCl
+import org.libsodium.jni.Sodium
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,5 +29,56 @@ class ExampleInstrumentedTest {
         val resp = Sphinx.respond(c.challenge, secret)
         val rwd = c.finish(resp)
         assertArrayEquals(rwd, Base64.decode(EXPECTED_BASIC_TEST, Base64.DEFAULT))
+    }
+
+    @Test
+    fun storageTest() {
+        NaCl.sodium()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val storage = AndroidCredentialStore(appContext)
+        val key = storage.key
+        assertEquals(key.size, Sodium.crypto_sign_secretkeybytes())
+        assertArrayEquals(key, storage.key)
+        assertEquals(storage.salt.size, SALT_BYTES)
+
+        val host1 = ByteArray(16) { 1 }
+        val host2 = ByteArray(16) { 2 }
+        val host1user1 = "foo"
+        val host1user2 = "bar"
+        val host2user1 = "baz"
+
+        assert(storage.getUsers(host1).isEmpty())
+        storage.deleteUser(host1, host1user1)
+
+        storage.cacheUser(host1, host1user1)
+        assertEquals(storage.getUsers(host1).size, 1)
+        assert(storage.getUsers(host1).contains(host1user1))
+
+        storage.cacheUser(host1, host1user2)
+        assertEquals(storage.getUsers(host1).size, 2)
+        assert(storage.getUsers(host1).containsAll(listOf(host1user1, host1user2)))
+
+        storage.cacheUser(host2, host2user1)
+        assertEquals(storage.getUsers(host1).size, 2)
+        assert(storage.getUsers(host1).containsAll(listOf(host1user1, host1user2)))
+        assertEquals(storage.getUsers(host2).size, 1)
+        assert(storage.getUsers(host1).contains(host1user2))
+
+        storage.deleteUser(host1, host1user1)
+        assertEquals(storage.getUsers(host1).size, 1)
+        assert(storage.getUsers(host1).contains(host1user2))
+
+        assertEquals(storage.host, "")
+        assertEquals(storage.port, 0)
+        assert(storage.serverPublicKey.isEmpty())
+
+        val host = "example.tld"
+        val port = 31337
+        val serverPublicKey = ByteArray(32) { 3 }
+        appContext.storeServerInfo(host, port, serverPublicKey)
+
+        assertEquals(storage.host, host)
+        assertEquals(storage.port, port)
+        assertArrayEquals(storage.serverPublicKey, serverPublicKey)
     }
 }
