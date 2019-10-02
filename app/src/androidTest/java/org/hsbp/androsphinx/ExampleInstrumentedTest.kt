1e7e30feec79d1fe2dde0865177af8ce3a01c9d3
András Veres-Szentkirályi <vsza@vsza.hu>
2019-10-02 10:36:42 +0200

    missing imports for REPL test

--- app/src/androidTest/java/org/hsbp/androsphinx/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/hsbp/androsphinx/ExampleInstrumentedTest.kt
@@ -10,6 +10,10 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.libsodium.jni.NaCl
 import org.libsodium.jni.Sodium
+import java.io.PrintWriter
+import java.lang.NumberFormatException
+import java.net.ServerSocket
+import java.util.*
 
 /**
  * Instrumented test, which will execute on an Android device.
