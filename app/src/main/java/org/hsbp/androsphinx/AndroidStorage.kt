25f5bfbb3b958afae699ac16f994d2868374547b
András Veres-Szentkirályi <vsza@vsza.hu>
2019-10-02 14:45:25 +0200

    use SQLite to store cache instead of text files

--- app/src/main/java/org/hsbp/androsphinx/AndroidStorage.kt
+++ app/src/main/java/org/hsbp/androsphinx/AndroidStorage.kt
@@ -1,17 +1,23 @@
 package org.hsbp.androsphinx
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.SharedPreferences
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.util.Base64
-import java.io.File
 import java.io.FileNotFoundException
 
 const val SALT_BYTES = 32
-const val BASE64_FLAGS = Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
 const val SHARED_PREFERENCES_NAME = "androsphinx"
 const val SHARED_PREFERENCES_KEY_HOST = "host"
 const val SHARED_PREFERENCES_KEY_PORT = "port"
 const val SHARED_PREFERENCES_KEY_SERVER_PK = "server_pk"
+
+const val DB_VERSION: Int = 1
+const val USERS_TABLE: String = "users"
+const val HOST_ID: String = "host_id"
+const val USERNAME: String = "username"
 
 // TODO encrypt before storage / decrypt after retrieval using Android Key Store
 
@@ -49,39 +55,40 @@ class AndroidCredentialStore(private val ctx: Context) : Protocol.CredentialStor
         get() = ctx.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
 
     override fun cacheUser(hostId: ByteArray, username: String) {
-        val filename = Base64.encodeToString(hostId, BASE64_FLAGS)
-        val f = File(ctx.cacheDir, filename)
-        if (f.exists()) f.appendText("\n$username") else f.writeText(username)
+        UserCache(ctx).writableDatabase.use { db ->
+            db.insertOrThrow(USERS_TABLE, null, ContentValues(1).apply {
+                put(HOST_ID, hostId)
+                put(USERNAME, username)
+            })
+        }
     }
 
     override fun getUsers(hostId: ByteArray): List<String> {
-        val filename = Base64.encodeToString(hostId, BASE64_FLAGS)
-        return try {
-            File(ctx.cacheDir, filename).readLines()
-        } catch (e: FileNotFoundException) {
-            emptyList()
+        UserCache(ctx).readableDatabase.use { db ->
+            db.rawQuery("SELECT $USERNAME FROM $USERS_TABLE WHERE $HOST_ID = x'${hostId.asHex}'", null).use { c ->
+                return generateSequence { if (c.moveToNext()) c.getString(0) else null }.toList()
+            }
         }
     }
 
     override fun deleteUser(hostId: ByteArray, username: String) {
-        val filename = Base64.encodeToString(hostId, BASE64_FLAGS)
-        try {
-            val oldFile = File(ctx.cacheDir, filename)
-            val users = oldFile.useLines { oldUserList ->
-                oldUserList.filter { it != username }.joinToString(separator = "\n").toByteArray()
-            }
-            if (users.size.toLong() == oldFile.length()) return
-
-            val newFile = File(ctx.cacheDir, "$filename.tmp")
-            newFile.outputStream().use {
-                it.write(users)
-            }
-            newFile.renameTo(oldFile)
-
-        } catch (e: FileNotFoundException) {
-            // nothing to delete
+        UserCache(ctx).writableDatabase.use { db ->
+            db.execSQL("DELETE FROM $USERS_TABLE WHERE $HOST_ID = x'${hostId.asHex}' AND $USERNAME = ?", arrayOf(username))
         }
     }
+}
+
+val ByteArray.asHex: String
+    get() = this.joinToString(separator = "") { it.toInt().and(0xFF).toString(16).padStart(2, '0') }
+
+class UserCache(context: Context) : SQLiteOpenHelper(context, "user_cache", null, DB_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("""CREATE TABLE IF NOT EXISTS $USERS_TABLE ($HOST_ID BLOB NOT NULL,
+            $USERNAME VARCHAR NOT NULL, PRIMARY KEY ($HOST_ID, $USERNAME));""")
+        db.execSQL("CREATE INDEX IF NOT EXISTS host_index ON $USERS_TABLE ($HOST_ID);")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, old: Int, new: Int) { /* nothing yet */ }
 }
 
 fun Context.storeServerInfo(host: String, port: Int, serverPublicKey: ByteArray) {
