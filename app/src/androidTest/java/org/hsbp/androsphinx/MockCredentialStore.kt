33f73252a72fcbce0cb7c942de5e2ab2997b0c58
András Veres-Szentkirályi <vsza@vsza.hu>
2020-05-12 15:07:38 +0200

    MockCredentialStore: removed unused user map

--- app/src/androidTest/java/org/hsbp/androsphinx/MockCredentialStore.kt
+++ app/src/androidTest/java/org/hsbp/androsphinx/MockCredentialStore.kt
@@ -1,14 +1,11 @@
 package org.hsbp.androsphinx
 
 import android.util.Base64
-import java.math.BigInteger
 
 @Suppress("SpellCheckingInspection")
 const val MOCK_KEY = "qshl2mv7A76V2io93b2oS9cjNTTupuWFf7z10aJP5LM="
 
 class MockCredentialStore : Protocol.CredentialStore {
-    private val users = mutableMapOf<BigInteger, MutableSet<String>>()
-
     override val host: String
         get() = "10.0.2.2"
 
