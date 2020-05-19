fa462328607384e00039f1aee98e2d0d8bbe5814
András Veres-Szentkirályi <vsza@vsza.hu>
2020-05-19 10:36:56 +0200

    allow URLs to be shared as text

--- app/src/main/java/org/hsbp/androsphinx/MainActivity.kt
+++ app/src/main/java/org/hsbp/androsphinx/MainActivity.kt
@@ -1,10 +1,12 @@
 package org.hsbp.androsphinx
 
+import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.libsodium.jni.NaCl
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +29,20 @@ class MainActivity : AppCompatActivity() {
 
         search_btn.setOnClickListener {
             onSearchRequested()
+        }
+
+        val i = intent
+        if (i.action == Intent.ACTION_SEND && i.type == "text/plain") {
+            val sharedText = i.getStringExtra(Intent.EXTRA_TEXT) ?: return
+            val url = try {
+                URL(sharedText)
+            } catch (e: Exception) {
+                return
+            }
+            startActivity(with(Intent(this, AccountsActivity::class.java)) {
+                action = Intent.ACTION_SEARCH
+                putExtra(SearchManager.QUERY, url.toString())
+            })
         }
     }
 }
