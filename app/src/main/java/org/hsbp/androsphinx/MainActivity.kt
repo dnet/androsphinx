585bf70f3c90ee337efd13a94b4e1f62e9ec46a6
András Veres-Szentkirályi <vsza@vsza.hu>
2022-11-02 14:41:04 +0100

    optimized keyboard for search bar
     - hint for hostname/URL-specific characters
     - disable autocorrect

--- app/src/main/java/org/hsbp/androsphinx/MainActivity.kt
+++ app/src/main/java/org/hsbp/androsphinx/MainActivity.kt
@@ -6,6 +6,7 @@ package org.hsbp.androsphinx
 import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.SearchView
@@ -47,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         val searchManager = getSystemService<SearchManager>()
         (menu.findItem(R.id.app_bar_search).actionView as SearchView).apply {
             setSearchableInfo(searchManager?.getSearchableInfo(componentName))
+            setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS)
         }
 
         return true
